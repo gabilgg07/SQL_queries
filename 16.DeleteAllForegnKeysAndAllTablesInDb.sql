d2440be123abd80ca1f121749e2ca4acb948bb6a
gabilgg07 <gabilgg07@gmail.com>
2023-01-15 22:50:15 +0400

    Delete all foregn keys and after all tables in database

---
+++ 16.DeleteAllForegnKeysAndAllTablesInDb.sql
@@ -0,0 +1,11 @@
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+
+DECLARE @sql NVARCHAR(max)=''
+
+SELECT @sql += ' Drop table ' + QUOTENAME(s.NAME) + '.' + QUOTENAME(t.NAME) + '; '
+FROM   sys.tables t
+       JOIN sys.schemas s
+         ON t.[schema_id] = s.[schema_id]
+WHERE  t.type = 'U'
+
+Exec sp_executesql @sql
